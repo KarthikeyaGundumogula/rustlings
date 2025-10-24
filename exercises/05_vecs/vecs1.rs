7d061e11f872da4402bfc59c7026c55c83df826c
KarthikeyaGundumogula <karthikeya200226@gamil.com>
2025-10-24 10:59:07 +0000

    completed the ex-05-01

--- exercises/05_vecs/vecs1.rs
+++ exercises/05_vecs/vecs1.rs
@@ -4,12 +4,18 @@ fn array_and_vec() -> ([i32; 4], Vec<i32>) {
     // TODO: Create a vector called `v` which contains the exact same elements as in the array `a`.
     // Use the vector macro.
     // let v = ???;
+    let v = vec![10,20,30,40];
 
     (a, v)
 }
 
 fn main() {
     // You can optionally experiment here.
+    let v = vec![10,20,30,40];
+    let s = &v[1];
+    println!("third element is {}",s);
+
+    // rust dereferences automatically or under the hood for Display and Debug traits
 }
 
 #[cfg(test)]
